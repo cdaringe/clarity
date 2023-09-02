1519105327f4c4561ba01eb14ddcf8f1d3b72def
cdaringe <cdaringe@gmail.com>
2023-09-02 11:41:38 -0700

    chore: add ci

---
+++ .github/contributing.md
@@ -0,0 +1,6 @@
+# contributing
+
+- install node using nvm/fnm. reference the `.nvmrc`. installing these tools is not covered here
+- install pnpm
+- `pnpm install`
+- go nuts
--- apps/ui/src/stories/SvgBounding.stories.tsx
+++ apps/ui/src/stories/SvgBounding.stories.tsx
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import React from "react";
 import {
   SvgBounding as Component,
   Props,
   useResponsiveLineCoords,
 } from "clarity-components";
+import React from "react";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Component> = {
